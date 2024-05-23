62d38088668aa363e677b7469e292e99c118e934
Nikita Polyakov <mr.lermontov228@yandex.ru>
2024-05-23 20:24:57 +0400

    few issues with dto

--- statistics-service/dto/src/main/java/ru/practicum/dto/StatisticsInputHitDto.java
+++ statistics-service/dto/src/main/java/ru/practicum/dto/StatisticsInputHitDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
@@ -16,16 +16,16 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder(toBuilder = true)
 public class StatisticsInputHitDto {
-    @NotNull(message = "Cannot be null or empty")
-    @Size(min = 1, max = 300)
+    @NotBlank(message = "Cannot be null or empty")
+    @Size(min = 1, max = 40)
     private String app;
-    @NotNull(message = "Cannot be null or empty")
-    @Size(min = 1, max = 300)
+    @NotBlank(message = "Cannot be null or empty")
+    @Size(min = 1, max = 40)
     private String uri;
-    @NotNull(message = "Cannot be null or empty")
-    @Size(min = 1, max = 300)
+    @NotBlank(message = "Cannot be null or empty")
+    @Size(min = 1, max = 40)
     private String ip;
-    @NotNull(message = "Cannot be null or empty")
+    @NotBlank(message = "Cannot be null or empty")
     @PastOrPresent
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
